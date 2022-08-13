8d0786de6f07eb190f5f4564b6d4be2998619e51
thisisyongsiang <ongys1992@gmail.com>
2022-08-14 02:50:38 +0800

    added routes to customer.js

--- customer.js
+++ customer.js
@@ -15,6 +15,7 @@ router.get("/customer/id",(req,res)=>{
         }
     )
 })
+
 router.get("/customer/all",(req,res)=>{
     db.connection.all('select * from customer',(err,results)=>{
         if(err){
@@ -26,4 +27,73 @@ router.get("/customer/all",(req,res)=>{
     })
 })
 
+router.get("/customer/id/items",(req,res)=>{
+    db.connection.all(`select * from item
+    where item.id in 
+    (select shop_order.itemId from shop_order
+        where shop_order.customerId='${req.query.cid}')`,(err,results)=>{
+            if (err) res.status(500).send('some error occured');
+            else{
+                res.status(200).send(results);
+            }
+        })
+
+})
+router.get("/shoporder/all",(req,res)=>{
+    db.connection.all(`select * from shop_order`,(err,results)=>{
+        if (err) res.status(500).send('some error occured');
+        else{
+            res.status(200).send(results);
+        }
+    })
+})
+
+router.get("/shoporder/id",(req,res)=>{
+    db.connection.all(`select * from shop_order where shop_order.id =' ${req.query.orderid}'`,(err,results)=>{
+        if (err) res.status(500).send('some error occured');
+        else{
+            res.status(200).send(results);
+        }
+    })
+})
+router.get("/shoporder/orderdate",(req,res)=>{
+    db.connection.all(`select * from shop_order 
+    where shop_order.order_date >='${req.query.startdate}'
+    and shop_order.order_date<='${req.query.enddate}'`,(err,results)=>{
+        if (err) res.status(500).send('some error occured');
+        else{
+            res.status(200).send(results);
+        }
+    })
+})
+
+router.get("/items/all",(req,res)=>{
+    db.connection.all(`select * from item`,(err,results)=>{
+        if (err) res.status(500).send('some error occured');
+        else{
+            res.status(200).send(results);
+        }
+    })
+})
+router.get("/items/id",(req,res)=>{
+    db.connection.all(`select * from item where item.id ='${req.query.itemid}'`,(err,results)=>{
+        if (err) res.status(500).send('some error occured');
+        else{
+            res.status(200).send(results);
+        }
+    })
+})
+router.get("/item/id/customer",(req,res)=>{
+    db.connection.all(`select * from customer 
+    where customer.id in 
+    (select shop_order.customerID from shop_order 
+        where shop_order.itemID = '${req.query.itemid}')`,(err,results)=>{
+            if(err){
+                res.status(500).send('some error occurred');
+            }
+            else{
+                res.status(200).send(results);
+            }
+        })
+})
 export{router};
